3e803234345c1f4ffd1cb5008c2272c476ad1ed9
Mansha Kaur <manshakaur007@gmail.com>
2026-06-30 12:26:44 +0530

    Add Genesis.py

---
+++ Foundations/Genesis.py
@@ -0,0 +1,24 @@
+# Genesis.py
+# Author- Mansha Kaur
+# Date- 28 June 2026
+# The beginning of my python journey
+print("==============================================")
+print("             G E N E S I S                   ")
+print("==============================================")
+print()
+
+print("Today isn't the day I become successful")
+print()
+print("Today is the day I decide to begin ")
+print()
+print("-----------------------------------------------")
+print("             Identity Registration            ")
+print("-----------------------------------------------")
+input("Press Enter to continue...")
+print()
+Name = input("Name: ")
+Dream = input("Dream: ")
+Reason = input("Reason for learning: ")
+Promise = input("One Promise: ")
+print()
+print("Every successful AI enterpreneur was once a single line coder. Success demands determination and dedication.")
